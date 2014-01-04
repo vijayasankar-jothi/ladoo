0d201f9414492bef9b3599a08a37cc104573f87a
vijayasankarj <vijayasankarj@freshdesk.com>
2014-01-04 08:02:16 +0530

    vj 3

--- src/com/droidfactory/ladoo/adapter/DrawerAdapter.java
+++ src/com/droidfactory/ladoo/adapter/DrawerAdapter.java
@@ -13,6 +13,8 @@ import com.droidfactory.ladoo.R;
 import com.droidfactory.ladoo.object.ParentObject;
 
 public class DrawerAdapter extends ArrayAdapter<ParentObject> { 
+	
+	int headerIcons[] = {R.drawable.ic_file_icon,R.drawable.ic_belllow,R.drawable.ic_camerahigh,R.drawable.ic_gallery,R.drawable.ic_crown,R.drawable.ic_old_camcorder,R.drawable.ic_cherrieshigh,R.drawable.ic_ic_celebs,R.drawable.ic_fb_small,R.drawable.ic_tweet_small};
 
 	public DrawerAdapter(Context mContext, ArrayList<ParentObject> mTripsArray) {
 		super(mContext, R.layout.drawer_row, mTripsArray);
@@ -37,6 +39,7 @@ public class DrawerAdapter extends ArrayAdapter<ParentObject> {
 	private void populateView(int position, View rowView) {
 		TaskViewHolder holder = (TaskViewHolder) rowView.getTag();
 		holder.parent_name.setText(this.getItem(position).parent_name);
+		holder.parent_name.setCompoundDrawablesWithIntrinsicBounds(0,headerIcons[position], 0, 0);
 	}
 	
 	class TaskViewHolder {
--- src/com/droidfactory/ladoo/database/Model.java
+++ src/com/droidfactory/ladoo/database/Model.java
@@ -178,7 +178,7 @@ public class Model {
 	public ArrayList<ParentObject> getCategoriesObjectArray() {
 		int i = 0;
 		ArrayList<ParentObject> parentsArray = new ArrayList<ParentObject>();
-		String[] categories_in_drawer = {"News Updates","Events","Photos","Galleries","Top Celebs","Trailers","Reviews","Natural Beauties","Like Us on Facebook", "Follow Us on twitter"};
+		String[] categories_in_drawer = {"News Updates","Events","Photos","Galleries","Top Celebs","Trailers","Reviews","Natural Beauties","Like Us", "Follow Us"};
 		String[] categories_in_title = {"News Updates","Events","Photos","Galleries","Top Celebs","Trailers","Reviews","Natural Beauties", "Like Us on Facebook","Follow Us on twitter"};
 		for (String cat_name : categories_in_drawer) {
 			String title = categories_in_title[i];
